c40ca48718a814e02e1e5b46a463e236067d11d6
facuagueria <fagueria@sugit.com.ar>
2024-10-05 00:15:25 -0300

    feat(tasks): getting project data with tasks

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { supabase, type Tables } from '@/lib/supabaseClient'
+import { QueryData } from '@supabase/supabase-js'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 
 usePageStore().pageData.title = 'My Tasks'
 
-const tasks = ref<Tables<'tasks'>[] | null>(null)
+const tasksWithProjectsQuery = supabase.from('tasks').select('*, projects (id, name, slug)')
+
+type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
+
+const tasks = ref<TasksWithProjects | null>(null)
 
 const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select('*')
+  const { data, error } = await tasksWithProjectsQuery
 
   if (error) console.log(error)
 
@@ -17,7 +22,7 @@ const getTasks = async () => {
 
 await getTasks()
 
-const columns: ColumnDef<Tables<'tasks'>>[] = [
+const columns: ColumnDef<TasksWithProjects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -47,10 +52,19 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     }
   },
   {
-    accessorKey: 'project_id',
-    header: () => h('div', { class: 'text-left' }, 'Project'),
+    accessorKey: 'projects',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+      return row.original.projects
+        ? h(
+            RouterLink,
+            {
+              to: `/projects/${row.original.projects.slug}`,
+              class: 'text-left font-medium hover:bg-muted block w-full'
+            },
+            () => row.original.projects?.name
+          )
+        : ''
     }
   },
   {
