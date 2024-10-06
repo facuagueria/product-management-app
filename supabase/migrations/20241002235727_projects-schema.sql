38be706eff603b08d57c74f9889272ff6799a363
facuagueria <fagueria@sugit.com.ar>
2024-10-06 17:46:52 -0300

    feat(projects): add description to projects schema

--- supabase/migrations/20241002235727_projects-schema.sql
+++ supabase/migrations/20241002235727_projects-schema.sql
@@ -9,6 +9,7 @@ create table
     id bigint primary key generated always as identity not null,
     name text not null,
     slug text unique not null,
+    description text,
     status current_status default 'in-progress' not null,
     collaborators text array default array[]::varchar[] not null,
     created_at timestamptz default now() not null
