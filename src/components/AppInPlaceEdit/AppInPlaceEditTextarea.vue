ce8b43a12da22416648dc7fd1d759b013d9e3b48
facuagueria <fagueria@sugit.com.ar>
2024-11-26 19:57:38 -0300

    feat(AppInPlaceEditTextarea): new component

---
+++ src/components/AppInPlaceEdit/AppInPlaceEditTextarea.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const value = defineModel<string>()
+
+defineEmits(['commit'])
+</script>
+
+<template>
+  <textarea
+    v-model="value"
+    class="w-full bg-transparent p-1 focus:rounded-md focus:border-none focus:bg-gray-800 focus:outline-none"
+    @blur="$emit('commit')"
+  />
+</template>
